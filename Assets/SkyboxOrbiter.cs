f91304aa70bd11a7f62cb1b3bac367e316bbec63
ISHTAR-kesadisan <kesadisan@gmail.com>
2016-10-05 16:39:50 +0700

    Finished menu system, updated project to use 5.4.1

---
+++ Assets/SkyboxOrbiter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyboxOrbiter : MonoBehaviour {
+
+    public float ySpeed = 0.0f;
+    public float x;
+    float y;
+    public float z;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        y += Time.deltaTime * ySpeed;
+
+        transform.rotation = Quaternion.Euler(x, y, z);
+    }
+}
